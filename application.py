b2d8c586550bb17113c27d7aad625a0f35a4d981
Oleksii Saiun <oleksiisaiun@gmail.com>
2022-03-22 23:48:35 -0500

    fix3.change color to black

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = app = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "<p><font color=black>Hello Oleksii Saiun.version3</font></p>"
+    return "<p><font color=black:q!>Hello Oleksii Saiun.version3.1</font></p>"
 
 if __name__ == "__main__":
     application.debug = True
